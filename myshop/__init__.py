36bfd042552f8daf604e965d16638857582d731c
Emima Vaipan <vaipane@cardiff.ac.uk>
2020-04-17 13:16:02 +0100

    remove cred

--- myshop/__init__.py
+++ myshop/__init__.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 Bootstrap(app)
-app.config['SECRET_KEY'] = '58be938844c3e73b40850dd2f170ed237a45588f70d5a825'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://c1743324:530337@csmysql.cs.cf.ac.uk:3306/c1743324'
+app.config['SECRET_KEY'] = ''
+app.config['SQLALCHEMY_DATABASE_URI']=''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db = SQLAlchemy(app)
@@ -22,8 +22,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 app.config['RECAPTCHA_USE_SSL']= False
-app.config['RECAPTCHA_PUBLIC_KEY']='6LfmRJkUAAAAAB-jCiByLXJW8QDlY5KKuXDpnN2L'
-app.config['RECAPTCHA_PRIVATE_KEY']='6LfmRJkUAAAAABD1z1hqeHJB7dqok1qxy8N2QEuw'
+app.config['RECAPTCHA_PUBLIC_KEY']=''
+app.config['RECAPTCHA_PRIVATE_KEY']=''
 app.config['RECAPTCHA_OPTIONS']= {'theme':'black'}
 
 
